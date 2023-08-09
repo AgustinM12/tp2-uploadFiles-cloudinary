374c0a080cb55bdfe4f87217d3309331430d4e03
AgustinM12 <agustinmazza12345@gmail.com>
2023-08-09 14:19:51 -0300

    se intalaron las dependencias de cloudinary y fileUpload

--- app.js
+++ app.js
@@ -4,11 +4,14 @@ const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const path = require('path');
-const fileUpload = require('express-fileupload');require('dotenv').config();
+const fileUpload = require('express-fileupload'); 
+const cloudinary = require('cloudinary').v2;
+
+require('dotenv').config();
 require('ejs')
 
 //Se prueba la conexion a la base de datps
-const { conectarDB } = require('./database'); 
+const { conectarDB } = require('./database');
 conectarDB();
 
 const port = process.env.PORT || 5000
@@ -22,20 +25,34 @@ app.use(helmet({
 }));
 app.use(morgan('dev'));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(fileUpload({
     limits: { fileSize: 50 * 1024 * 1024 },
-  }));
+}));
+
+cloudinary.config({
+    cloud_name: 'dtneznsas',
+    api_key: '897753129858681',
+    api_secret: 'kX_njYFJTubb3joz-irDktvo-8U'
+});
 
 // Routes
-app.use('/api', require('./routes/imagen.routes'));
+// app.use('/', require('./routes/imagen.routes'));
+
+app.get('/', (req, res) => {
+    res.send("Hola mundo!")
+})
+
+app.post('/', (req, res) => {
+    console.log(req.files)
+})
 
 //Si la petición no coincide con ninguna de las rutas declaradas, mostrar error 404
-app.use((req, res, next) => {
-    return res.status(404).render('404')
-});
+// app.use((req, res, next) => {
+//     return res.status(404).render('404')
+// });
 
 
 // Arrancar el servidor
